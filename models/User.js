14b7070076958d3648d01209e6e821951e18ece8
Thenujana <chenarathenujana@gmail.com>
2025-07-25 20:33:15 +0530

    updates in the model

--- models/User.js
+++ models/User.js
@@ -5,3 +5,5 @@ const userSchema=new mongoose.Schema({
       email:{type:String,required:true},
        image:{type:String,required:true},
 })
+const User=mongoose.model('User',userSchema)
+export default User;
