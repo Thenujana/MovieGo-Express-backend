b2285ab0dd9d63494b2829ca660362077ed6bb5a
Thenujana <chenarathenujana@gmail.com>
2025-07-29 17:13:41 +0530

    new chnages

--- inngest/index.js
+++ inngest/index.js
@@ -18,4 +18,30 @@ const syncUserCreation=inngest.createFunction(
     }
 )
 //deleteing the user in the databse 
-export const functions = [syncUserCreation];
+const syncUserDeletion=inngest.createFunction(
+    {id:'delete-user-from-clerk'},
+    {event:'clerk/user.deleted'},
+    async({event})=>{
+        const{id}=event.data
+        await User.findByIdAndDelete(id)
+        
+    }
+)
+//updating the user in the databse 
+const syncUserUpdation=inngest.createFunction(
+    {id:'update-user-from-clerk'},
+    {event:'clerk/user.updated'},
+    async({event})=>{
+       const{id,first_name,last_name,email_address,image_url}=event.data
+       const userData={
+        _id:id,
+        email:email_address[0].email_address,
+        name:first_name +' '+last_name,
+        image:image_url
+       }
+await User.findByIdAndUpdate(id,userData)
+    }
+)
+
+
+export const functions = [syncUserCreation,syncUserDeletion];
