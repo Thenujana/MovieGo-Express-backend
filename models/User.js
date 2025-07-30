5c0029b5433fb8e684ffab153fb19ab25f8b4d3b
Thenujana <chenarathenujana@gmail.com>
2025-07-30 22:54:14 +0530

    changes

--- models/User.js
+++ models/User.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 const userSchema=new mongoose.Schema({
-    _id:{type:String,required:true},
-     name:{type:String,required:true},
-      email:{type:String,required:true},
-       image:{type:String,required:true},
+    _id:{type:String,required:false},
+     name:{type:String,required:false},
+      email:{type:String,required:false},
+       image:{type:String,required:false},
 })
 const User=mongoose.model('User',userSchema)
 export default User;
